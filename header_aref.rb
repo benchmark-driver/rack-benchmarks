66de1f3804f28791f02c6c8ead1f5b4ec2775384
Takashi Kokubun <takashikkbn@gmail.com>
2019-11-15 00:42:44 -0800

    Add header_aref.rb

--- header_aref.rb
+++ header_aref.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
-require 'rack/mock'
+require 'rack/utils'
 
 def run(loop_count, quiet: false)
+  header = Rack::Utils::HeaderHash.new
+
   before = Process.clock_gettime(Process::CLOCK_MONOTONIC)
   i = 0
   while i < loop_count
-    Rack::MockRequest.env_for('/', method: 'GET')
+    header['Content-Type']
     i += 1
   end
   after = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-  puts "==> #{after-before} (#{loop_count} times)"
+  puts "==> #{after-before} (#{loop_count} times)" unless quiet
 end
 
 run(10000, quiet: true)
 if RubyVM::MJIT.enabled?
   RubyVM::MJIT.pause
 end
-run(300000)
+run(8000000)
